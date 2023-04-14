77982293b49ebe173361115349f0c6491facf85d
Bankde <bankde@hotmail.com>
2023-04-14 23:16:27 +0700

    python - improve repl test

--- Volpy/src/python/volpy_repl_test.py
+++ Volpy/src/python/volpy_repl_test.py
@@ -11,48 +11,62 @@ import argparse
 import logging, sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-async def test_repl(i=0):
-    print("Test distributing task")
-    def task1(i):
-        return i+1
-    def task2(i):
-        return i+2
-    Volpy.registerRemote(task1)
-    Volpy.registerRemote(task2)
-    print("=====================")
-    if i==1: return
+TEST_COUNT = 5
+async def test_repl(i):
+    match i:
+        case 1:
+            print("Test distributing task")
+            def task1(i):
+                return i+1
+            def task2(i):
+                return i+2
+            Volpy.registerRemote(task1)
+            Volpy.registerRemote(task2)
+            print("=====================")
 
-    print("Test simple task, should return 4")
-    def test(i):
-        return i+1
-    Volpy.registerRemote(test)
-    a = test.remote(3)
-    print(a.get())
-    print("=====================")
-    if i==2: return
+        case 2:
+            print("Test simple task, should return 4")
+            def test(i):
+                return i+1
+            Volpy.registerRemote(test)
+            a = test.remote(3)
+            print(a.get())
+            print("=====================")
 
-    print("Test run 10 times (blocking)")
-    for i in range(10):
-        print(test.remote(i).get())
-    print("=====================")
-    if i==3: return
+        case 3:
+            print("Test run 10 times (blocking)")
+            for i in range(10):
+                print(test.remote(i).get())
+            print("=====================")
 
-    print("Test non-blocking longTask")
-    import time
-    def longTask(i):
-        time.sleep(5)
-        return i+1
-    Volpy.registerRemote(longTask)
-    ts = []
-    for i in range(10):
-        try:
-            ts.append(longTask.remote(i))
-        except Exception:
-            pass
-    for t in ts:
-        print(t.get())
-    print("=====================")
-    if i==4: return
+        case 4:
+            print("Test non-blocking longTask")
+            import time
+            def longTask(i):
+                time.sleep(5)
+                return i+1
+            Volpy.registerRemote(longTask)
+            ts = []
+            for i in range(10):
+                try:
+                    ts.append(longTask.remote(i))
+                except Exception:
+                    pass
+            for t in ts:
+                print(t.get())
+            print("=====================")
+
+        case 5:
+            print("Test imported module [ret: 7]")
+            import numpy as np
+            def findMax(i):
+                return np.max(i)
+            Volpy.registerRemote(findMax)
+            findMax.remote(np.array([1,3,7,2])).get()
+            print("=====================")
+
+def csv_list(s):
+    return [int(x) for x in s.split(",")]
 
 if __name__ == '__main__':
     '''
@@ -61,7 +75,7 @@ if __name__ == '__main__':
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument("--rayletipc_addr", type=str, required=True, help="Specify raylet IPC address")
-    parser.add_argument("-i", type=int, default=0, help="Specify testing task")
+    parser.add_argument("-i", type=csv_list, default=0, help="Specify testing task (e.g. -i 1 or -i 1,2)")
     args = parser.parse_args()
     config.merge(args)
 
@@ -71,5 +85,8 @@ if __name__ == '__main__':
     loop.run_until_complete(ipc_caller.waitReady())
     volpy_task_manager.TaskManager().setup(ipc_caller)
 
-    loop.run_until_complete(test_repl(config.i))
+    if len(config.i) == 1 and config.i[0] == 0:
+        config.i = list(range(1,TEST_COUNT+1))
+    for i in config.i:
+        loop.run_until_complete(test_repl(i))
     loop.stop()
