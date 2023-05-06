1bdb4862fb5bb23544bbf2b74f80af2b6f9b59fc
Bankde <bankde@hotmail.com>
2023-05-07 02:00:09 +0700

    test - fix distributed sort test, work good now

--- Volpy/src/python/volpy_repl_test_dissort.py
+++ Volpy/src/python/volpy_repl_test_dissort.py
@@ -15,8 +15,11 @@ import numpy as np
 import time
 
 # MagicSortNumPerCore = (10**6)*3 # Magic number from testing (should be around 0.1s-1s)
-MagicSortNumPerCore = (10**5)*3 # Max memory for grpc message
+MagicSortNumPerCore = (10**4)*3 # Max memory for grpc message
 TotalSortNum = MagicSortNumPerCore*2
+
+# Largest grpc: 4194304 bytes
+# Largest websocket: 1048576 bytes
 
 def partition(collection):        
     # Use the last element as the first pivot
@@ -29,22 +32,23 @@ def partition(collection):
             lesser.append(element)
     return lesser, pivot, greater
 
-def quick_sort_distributed(collection):
+async def quick_sort_distributed(collection):
     # Tiny tasks are an antipattern. 
     # Thus, in our example we have a "magic number" to 
     # toggle when distributed recursion should be used vs
     # when the sorting should be done in place. The rule
     # of thumb is that the duration of an individual task
     # should be at least 1 second.
-    print(len(collection), MagicSortNumPerCore)
     if len(collection) <= MagicSortNumPerCore:  # magic number (Lowered for unittest)
         return sorted(collection)
     else:
         lesser, pivot, greater = partition(collection)
-        lesser = quick_sort_distributed.remote(lesser).get()
-        greater = quick_sort_distributed.remote(greater).get()
+        lesser_ref = await quick_sort_distributed.remote(lesser)
+        greater_ref = await quick_sort_distributed.remote(greater)
+        lesser = await lesser_ref.get()
+        greater = await greater_ref.get()
         return lesser + [pivot] + greater
-    
+
 def quick_sort_single_core(collection):
     if len(collection) <= MagicSortNumPerCore:  # magic number (Lowered for unittest)
         return sorted(collection)
@@ -57,10 +61,11 @@ def quick_sort_single_core(collection):
 async def test_repl(i=0):
     np.random.seed(0)
     unsorted = np.random.randint(100000, size=(TotalSortNum)).tolist()
-    Volpy.registerRemote(quick_sort_distributed)
+    await Volpy.registerRemote(quick_sort_distributed)
     t1 = time.time()
-    sort = quick_sort_distributed.remote(unsorted).get()
+    sort = await (await quick_sort_distributed.remote(unsorted)).get()
     t2 = time.time()
+    test_data = [sort[0], sort[10], sort[100], sort[1000], sort[-1]]
     print(f"Time taken (Volpy): {(t2-t1)*1000} ms")
 
     np.random.seed(0)
@@ -68,7 +73,11 @@ async def test_repl(i=0):
     t1 = time.time()
     sort = quick_sort_single_core(unsorted)
     t2 = time.time()
+    verf_data = [sort[0], sort[10], sort[100], sort[1000], sort[-1]]
     print(f"Time taken (1 core): {(t2-t1)*1000} ms")
+
+    print("==Verify==")
+    print(np.array_equiv(test_data, verf_data))
 
 if __name__ == '__main__':
     '''
